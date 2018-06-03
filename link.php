9891f1c6017a4918349b22aa917db883cc988627
sylviavb86 <39881427+sylviavb86@users.noreply.github.com>
2018-06-03 17:38:27 +0200

    First land up page (udemy course)

---
+++ link.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Thriving cities events</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+  <script src="bootstrap.min.js" type="text/javascript"/></script>
+  <script src="bootstrap.bundle.js" type="text/javascript"/></script>
+</head>
+<body>
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="http://sylvia.slyermedia.eu/udemy/challenge/challenge.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://sylvia.slyermedia.eu/udemy/challenge/link.php">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="http://sylvia.slyermedia.eu/udemy/challenge/challenge.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="http://sylvia.slyermedia.eu/udemy/challenge/action.php">Action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="http://sylvia.slyermedia.eu/udemy/challenge/action2.php">Action2</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+  <p>
+    Link
+  </p>
+<!--   uitklapmenu met maanden naar links, daaronder soorten evenementen naar links
+  op de site uitklapbaar deel met evenementen op maand gesorteerd per maand uit te klappen. Daarna hetzelfde maar per categorie. -->
+</body>
+</html>
